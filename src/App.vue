ea0508caa18acd78f05ebce00d43d38dc0723df4
Shafiur Rahman <msruchchhas.dev@gmail.com>
2021-03-08 00:44:59 +0600

    Setup project with Vue CLI

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container-scroller">
+        <Navbar/>
+
+        <Sidebar/>
+
+        <div class="container-fluid page-body-wrapper">
+            <div class="main-panel">
+                <div class="content-wrapper">
+
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/layouts/Navbar";
+import Sidebar from "./components/layouts/Sidebar";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      Navbar,
+      Sidebar
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/css/style.css'
+import './assets/js/bootstrap.bundle.min'
 
 createApp(App).mount('#app')
